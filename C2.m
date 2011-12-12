ef96b5744532aca1885a35f8d9da9d33f44c63e0
Josh Rule <joshrule@joshrule.com>
2011-12-12 13:14:21 +0700

    inital addition of Jake & Justyna's code

--- C2.m
+++ C2.m
@@ -1,4 +1,40 @@
-function [c2,s2,c1,s1] = C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,s2Target,c1)
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+%
+% Copyright (c) 2010 by Jacob G. Martin - All Rights Reserved
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are strictly prohibited.
+%
+% The name of Jacob G. Martin may not be used to endorse or promote products
+% derived from or associated with this software without specific prior written permission.
+% 
+% No products may be derived from or associated with this software without specific permission.
+%
+% This software is provided "AS IS," without a warranty of any
+% kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+% WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+% EXCLUDED. Jacob G. Martin SHALL NOT BE LIABLE FOR ANY
+% DAMAGES OR LIABILITIES SUFFERED BY ANY ORGANIZATION OR ITS
+% LICENSEES AS A RESULT OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THIS
+% SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL Jacob G. Martin BE LIABLE
+% FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT,
+% SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+% CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
+% THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF Jacob G. Martin HAS
+% BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES AND THE USER OF THIS CODE
+% AGREES TO HOLD Jacob G. Martin HARMLESS THEREFROM.
+%
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+
+
+function [c2,s2,c1,s1,bestbandnums,bestlocations] = C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,s2Target,c1,patchSize,s2c1prune)
 %function [c2,s2,c1,s1] = C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,s2Target,c1)
 %
 % given an image extracts layers s1 c1 s2 and finally c2
@@ -19,8 +55,12 @@ function [c2,s2,c1,s1] = C2(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL,s2Target,
 %
 % See also C1
 
+% c1 = [];
+s1 = [];
+bestbandnum = [];
+bestlocation = [];
 
-if nargin<8
+if nargin<8 || length(c1) == 0
   [c1,s1] = C1(stim,filters,fSiz,c1SpaceSS,c1ScaleSS,c1OL);
 end
 
@@ -28,8 +68,8 @@ nbands = length(c1);
 c1BandImage = c1;
 nfilts = size(c1{1},3);
 n_rbf_centers = size(s2Target,2);
-L = size(s2Target,1) / nfilts;
-PatchSize = [L^.5,L^.5,nfilts];
+
+PatchSize = [patchSize(1,1),patchSize(2,1),nfilts];
 
 s2 = cell(n_rbf_centers,1);
 
@@ -41,15 +81,30 @@ for iCenter = 1:n_rbf_centers
   Patch = reshape(s2Target(:,iCenter),PatchSize);
   s2{iCenter} = cell(nbands,1);
   for iBand = 1:nbands
-     s2{iCenter}{iBand} = windowedPatchDistance(c1BandImage{iBand},Patch);  
+     s2{iCenter}{iBand} = windowedPatchDistance(c1BandImage{iBand},Patch,s2c1prune);  
   end
 end
 
 %Build c2:
 % calculate minimum distance (maximum stimulation) across position and scales
 c2 = inf(n_rbf_centers,1);
+
+bestlocations = zeros(n_rbf_centers,2);
+bestbandnums = zeros(n_rbf_centers,1);
 for iCenter = 1:n_rbf_centers
+  bestbandnum = -1;
   for iBand = 1:nbands
-     c2(iCenter) = min(c2(iCenter),min(min(s2{iCenter}{iBand})));
+     [minval minlocation] = min(s2{iCenter}{iBand}(:));
+     oldvalue = c2(iCenter);
+     [c2(iCenter)] = min(c2(iCenter),minval);
+     if bestbandnum == -1 || oldvalue > c2(iCenter)
+        bestbandnum = iBand;
+        [bestlocation(1,1) bestlocation(1,2)] = ind2sub(size(s2{iCenter}{iBand}),minlocation);
+     end
   end
+  bestlocations(iCenter,1) = bestlocation(1,1);
+  bestlocations(iCenter,2) = bestlocation(1,2);
+  bestbandnums(iCenter,1) = bestbandnum;
+  
+  % [r c] = ind2sub([10 10], 31)
 end
--- windowedPatchDistance.m
+++ windowedPatchDistance.m
@@ -1,28 +1,105 @@
-function D = windowedPatchDistance(img,patch)
-%function D = windowedPatchDistance(img,patch)
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
 %
-% computes the euclidean distance between patch and all crops of img of
-% similar size.
+% Copyright (c) 2010 by Jacob G. Martin - All Rights Reserved
 %
-% sum_over_p(W(p)-I(p))^2 is factored as
-% sum_over_p(W(p)^2) + sum_over_p(I(p)^2) - 2*(W(p)*I(p));
+% Redistribution and use in source and binary forms, with or without
+% modification, are strictly prohibited.
 %
-% img and patch must have the same number of channels
+% The name of Jacob G. Martin may not be used to endorse or promote products
+% derived from or associated with this software without specific prior written permission.
+% 
+% No products may be derived from or associated with this software without specific permission.
+%
+% This software is provided "AS IS," without a warranty of any
+% kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
+% WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+% EXCLUDED. Jacob G. Martin SHALL NOT BE LIABLE FOR ANY
+% DAMAGES OR LIABILITIES SUFFERED BY ANY ORGANIZATION OR ITS
+% LICENSEES AS A RESULT OF OR RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THIS
+% SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL Jacob G. Martin BE LIABLE
+% FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT,
+% SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+% CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
+% THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF Jacob G. Martin HAS
+% BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES AND THE USER OF THIS CODE
+% AGREES TO HOLD Jacob G. Martin HARMLESS THEREFROM.
+%
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
+% PROPRIETARY INFORMATION
 
-assert(size(img,3) == size(patch,3), ...
-  'patch and image must have the same number of layers: %d != %d', size(img,3), size(patch,3));
 
-Psqr = sum(sum(sum(patch.^2)));
-imgsq = sum((img.^2),3); % why merely sum over the third dimension (rgb)?
-s = size(patch);
-sumRadius = [ceil(s(2)/2)-1, ceil(s(1)/2)-1, floor(s(2)/2), floor(s(1)/2)];
-imgsq = sumFilter(imgsq,sumRadius);
-PI = zeros(size(imgsq));
+    function D = WindowedPatchDistance(Im,Patch,s2c1prune)
+    %function D = WindowedPatchDistance(Im,Patch)
+    %
+    %computes the euclidean distance between Patch and all crops of Im of
+    %similar size.
+    %
+    % sum_over_p(W(p)-I(p))^2 is factored as
+    % sum_over_p(W(p)^2) + sum_over_p(I(p)^2) - 2*(W(p)*I(p));
+    %
+    % Im and Patch must have the same number of channels
+    %
 
-for i = 1:size(img,3)
-    PI = PI + conv2(img(:,:,i),patch(:,:,i), 'same');
+
+
+    dIm = size(Im,3);
+    dPatch = size(Im,3);
+    if(dIm ~= dPatch)
+      fprintf('The patch and image must be of the same number of layers');
+    end
+    % patches should be normalized.  1.0 0.9 0.8 0.7
+
+    prunepatches = s2c1prune > 0;
+    s = size(Patch);
+    s(3) = dIm;
+    if prunepatches
+        Psqr = 0;
+        for i = 1:dIm
+            % if prune, only keep the patch values that are greater than the patchpruneamount
+            indices(:,:) = Patch(:,:,i) > s2c1prune;
+            Psqr = Psqr + sum(sum(sum( indices(:,:) .* Patch(:,:,i).^2)));
+        end
+    else
+        Psqr = sum(sum(sum(Patch.^2)));
+    end
+    Imsq = Im.^2;
+    Imsq = sum(Imsq,3);
+    sum_support = [ceil(s(2)/2)-1,ceil(s(1)/2)-1,floor(s(2)/2),floor(s(1)/2)];
+    Imsq = sumFilter(Imsq,sum_support);
+    PI = zeros(size(Imsq));
+    nPI = zeros(size(Imsq));
+
+
+%     patchpruneamount = 0.10;
+    [rows cols z] = size(Patch);
+    indices = zeros(rows,cols);
+    for i = 1:dIm
+        if prunepatches
+            % if prune, only keep the patch values that are greater than the patchpruneamount
+            indices(:,:) = Patch(:,:,i) > s2c1prune;
+             PI = PI + conv2(Im(:,:,i),indices(:,:) .* Patch(:,:,i), 'same');
+%             PI = PI + cudaconv2(Im(:,:,i),indices(:,:) .* Patch(:,:,i));
+        else
+             PI = PI + conv2(Im(:,:,i),Patch(:,:,i), 'same');
+%              newim = single(Im(:,:,i));
+%              newp = single(Patch(:,:,i));
+%              nPI = nPI + conv2(newim,newp, 'same');
+%              PI2 = PI + convnfft(Im(:,:,i),Patch(:,:,i), 'same');  % slower!
+%             PI = PI + cudaconv2(Im(:,:,i),Patch(:,:,i));
+        end
+
+    end
+
+    tempval = Imsq - 2 * PI + Psqr;
+    tempval(tempval < 0) = 0;
+    D = sqrt(tempval) + 10^-10;
+    if ~isreal(D)
+       disp(['NOT REAL DATA!!!!!']); 
+    end
 end
-
-tempval = imgsq - 2 * PI + Psqr;
-tempval(tempval < 0) = 0;
-D = sqrt(tempval) + 10^-10; % what is 1E-10 here for?
