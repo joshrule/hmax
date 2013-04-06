01b4836612fee7fed5e2f051b0977c56df613d76
Josh Rule <rsj28@georgetown.edu>
2013-04-06 07:54:57 +0700

    add C1-caching

--- c1rFromCells.m
+++ c1rFromCells.m
@@ -1,7 +1,8 @@
-function c1r = c1rFromCells(imgs,params)
-% c1r = c1rFromCells(imgs,filters,filterSizes,c1OL,c1Space,c1Scale)
+function c1cell = c1rFromCells(imgs,params)
+% c1cell = c1rFromCells(imgs,filters,filterSizes,c1OL,c1Space,c1Scale)
 %
 % A C1.m wrapper. For each image in 'imgs', extract all C1 layer responses.
+% Extra complexity comes from caching
 %
 % args:
 %
@@ -9,15 +10,28 @@ function c1r = c1rFromCells(imgs,params)
 %
 %     params: see getParamsForCategory.m
 %
-% returns: c1r, a cell array of C1 responses, 1 set per image
+% returns: c1cell, a cell array of C1 responses, 1 set per image
 
-c1r = cell(1,length(imgs));
-parfor i = 1:length(imgs)
-    img = double(resizeImage(grayImage(imread(imgs{i})),params.maxSize));
-    c1r{i} = C1(img,...
+c1cell = cell(1,length(imgs));
+for i = 1:length(imgs)
+    c1r = {};
+    dummy = struct;
+    cacheFile = [imgs{i} '.c1.mat'];
+    if exist(cacheFile,'file')
+        loaded = load(cacheFile,'c1r');
+    end
+    if exist('loaded','var') && isfield(loaded,'c1r')
+        c1r = loaded(1).c1r;
+    else
+        img = double(resizeImage(grayImage(imread(imgs{i})),params.maxSize));
+        c1r = C1(img,...
                 params.filters,...
                 params.filterSizes,...
                 params.c1Space,...
                 params.c1Scale,...
                 params.c1OL);
+        dummy.c1r = c1r;
+        saveWrapper(cacheFile,dummy);
+    end
+    c1cell{i} = c1r;
 end
